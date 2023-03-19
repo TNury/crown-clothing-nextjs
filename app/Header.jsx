3562588e3cdc4dddfc5da880922f24cd261b5827
Yuri Souza <yurdesou@gmail.com>
2023-03-19 16:01:26 -0300

    fix(Header): Increased its z-index

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,14 +1,23 @@
+import Link from 'next/link';
+
 import Image from 'next/image';
 
 export const Header = () => {
   return (
-    <header className='pl-4 w-full h-20 flex items-center justify-between fixed top-0 bg-white md:pl-16 md:pr-14'>
+    <header className='pl-4 w-full h-20 flex items-center justify-between fixed top-0 z-20 bg-white md:pl-16 md:pr-14'>
       <Image src='/assets/logo.svg' width={50} height={50} alt='brand_logo' />
       <div className='flex gap-2 items-center'>
-        <a className='p-2 text-base cursor-pointer'>SHOP</a>
+        <Link href='/shop' className='p-2 text-base cursor-pointer'>
+          SHOP
+        </Link>
         <a className='p-2 text-base cursor-pointer'>SIGN IN</a>
         <div className='p-2 cursor-pointer flex items-center justify-center relative'>
-          <Image src='/assets/shopping-bag.svg' width={24} height={24} alt='bag_icon' />
+          <Image
+            src='/assets/shopping-bag.svg'
+            width={24}
+            height={24}
+            alt='bag_icon'
+          />
           <span className='text-[10px] font-bold absolute top-4'>0</span>
         </div>
       </div>
