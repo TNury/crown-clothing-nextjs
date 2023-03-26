ad988d4a0eaf54c0891fbdd1cc2ec6ea3f0c2272
Yuri Souza <yurdesou@gmail.com>
2023-03-26 11:20:46 -0300

    feat(ShopPage): Implemented data from firestore

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -1,17 +1,19 @@
+import { fetchShopPageEntries } from 'services/firebase';
+
 import { ProductBox } from './Product';
 
-import { staticData } from './staticData';
+const ShopPage = async () => {
+  const shopPageEntries = await fetchShopPageEntries();
 
-const ShopPage = () => {
   return (
     <main id='shop' className='px-4 py-16 flex flex-col gap-16 md:px-16'>
-      {staticData.map((entry) => (
-        <section key={entry.id} className='flex flex-col gap-4'>
+      {shopPageEntries.map((entry, index) => (
+        <section key={index} className='flex flex-col gap-4'>
           <h2 className='text-3xl font-bold uppercase cursor-pointer'>
             {entry.title}
           </h2>
           <div className='flex gap-4 flex-wrap'>
-            {entry.products.slice(0, 4).map((productData) => (
+            {entry.items.map((productData) => (
               <div key={productData.id} className='h-[25vw] flex-auto'>
                 <ProductBox productData={productData} />
               </div>
