a91ad60ecdf30ed837ce765b40e39d84ce66ef9d
Yuri Souza <yurdesou@gmail.com>
2023-03-26 10:15:47 -0300

    fix(home): Fixed all categories showing having title of 'hats'

--- app/page.jsx
+++ app/page.jsx
@@ -33,7 +33,10 @@ const categories = [
 
 const Home = () => {
   return (
-    <div id='home' className='min-h-[calc(100vh-5rem)] flex items-center justify-center'>
+    <div
+      id='home'
+      className='min-h-[calc(100vh-5rem)] flex items-center justify-center'
+    >
       <section
         id='categories'
         className='px-4 w-full flex gap-4 flex-wrap justify-between md:px-16'
@@ -48,7 +51,7 @@ const Home = () => {
               className='w-full h-full object-cover -z-10 absolute group-hover:scale-110 transition-all duration-1000'
             />
             <div className='h-24 px-6 flex flex-col items-center gap-4 bg-white border border-black opacity-70 group-hover:opacity-90 transition-all duration-500'>
-              <h2 className='text-2xl font-bold'>HATS</h2>
+              <h2 className='text-2xl uppercase font-bold'>{entry.title}</h2>
               <p>Shop Now</p>
             </div>
           </div>
