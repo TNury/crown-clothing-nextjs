9e8bdd1310a5922aeaac915dd72217024a30af00
Yuri Souza <yurdesou@gmail.com>
2023-04-07 11:49:39 -0300

    feat: Implemented redux-persist

--- redux/store.js
+++ redux/store.js
@@ -1,11 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
+
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 
-const store = configureStore({
-  reducer: rootReducer,
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
-export default store;
+export const persistor = persistStore(store);
