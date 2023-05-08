701a0ed2d2b78e4385e5adc52d182700419172d2
Yuri Souza <yurdesou@gmail.com>
2023-05-08 12:34:59 -0300

    WIP - Shopify Integration: Integrated shopify on shop page

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -1,19 +1,22 @@
-import { fetchShopPageEntries } from '@services/firebase';
-
 import { ProductBox } from '@/components/ProductBox';
 
+import fetchFromAPI from '@services/api';
+
 const ShopPage = async () => {
-  const shopPageEntries = await fetchShopPageEntries();
+  const { collections: shopPageQuery } = await fetchFromAPI(
+    'services/queries/collections.graphql',
+    'shopPageQuery'
+  );
 
   return (
     <main id='shop' className='px-4 py-16 flex flex-col gap-16 md:px-16'>
-      {shopPageEntries.map((entry, index) => (
+      {shopPageQuery.nodes.map((entry, index) => (
         <section key={index} className='flex flex-col gap-4'>
           <h2 className='text-3xl font-bold uppercase cursor-pointer'>
             {entry.title}
           </h2>
           <div className='flex gap-4 flex-wrap'>
-            {entry.items.map((productData) => (
+            {entry.products.nodes.map((productData) => (
               <div key={productData.id} className='h-[25vw] flex-auto'>
                 <ProductBox productData={productData} />
               </div>
