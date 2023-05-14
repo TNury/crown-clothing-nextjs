cc1a6b6e6cab5674e0576f3905801b4e15be0e65
Yuri Souza <yurdesou@gmail.com>
2023-05-14 19:55:50 -0300

    feat(Button): Implemented new variant prop on button

--- app/components/ui/button/Button.jsx
+++ app/components/ui/button/Button.jsx
@@ -1,16 +1,23 @@
-import clsx from 'clsx';
+'use client';
 
 export const Button = (props) => {
-  const { className, children, ...restOfProps } = props;
+  const { variant, className, children, ...restOfProps } = props;
+
+  const handleClassNames = () => {
+    const standardClasses =
+      'w-full p-4 flex items-center justify-center transition-all duration-200';
+
+    switch (variant) {
+      case 'secondary':
+        return `${standardClasses} bg-opaque-white-90 border border-black text-base font-bold uppercase text-black hover:bg-black hover:text-white`;
+      case 'primary':
+      default:
+        return `${standardClasses} bg-black border border-black text-base font-bold uppercase text-white hover:bg-white hover:text-black`;
+    }
+  };
 
   return (
-    <button
-      {...restOfProps}
-      className={`w-full p-4 mt-auto flex items-center justify-center 
-        bg-black border border-black text-base font-bold 
-        uppercase transition-all duration-200 text-white 
-        hover:bg-white hover:text-black ${clsx(className && className)}`}
-    >
+    <button {...restOfProps} className={handleClassNames()}>
       {children}
     </button>
   );
