ffe023565bffd2b4865ce5c0984658c481a36b6c
Yuri Souza <yurdesou@gmail.com>
2023-05-17 20:57:43 -0300

    feat(AddToCart): Successfully implemented server action

--- app/components/ui/specialized/add-to-cart/AddToCart.jsx
+++ app/components/ui/specialized/add-to-cart/AddToCart.jsx
@@ -1,12 +1,24 @@
-'use client';
+import { cookies } from 'next/headers';
 
 import callAPI from '@services/api';
 
 import { Button } from '@/components/ui/generic/button/Button';
 
+/*
+  Current problem:
+
+  When we call triggerItemAddition on /shop/categorySlug,
+  we get a 405 error. It works no shop/.
+
+  The reason why this is happening is due to generate static params
+*/
 const AddToCart = ({ productData }) => {
-  const triggerItemAddition = async () => {
-    const merchandiseId = productData.variants.nodes[0].id;
+  async function triggerItemAddition(arg) {
+    'use server';
+
+    // If you're trying to iterate through an object defined outside
+    // the scope of the server action, you need to access it like this.
+    const merchandiseId = productData['variants']['nodes'][0]['id'];
 
     const response = await callAPI(
       'services/queries/cart.graphql',
@@ -16,9 +28,15 @@ const AddToCart = ({ productData }) => {
         merchandiseId,
       }
     );
-  };
 
-  return <Button onClick={triggerItemAddition}>Add to cart</Button>;
+    cookies().set('cartId', response.cartCreate.cart.id, { secure: true });
+  }
+
+  return (
+    <form action={triggerItemAddition}>
+      <Button type='submit'>Add to cart</Button>
+    </form>
+  );
 };
 
 export default AddToCart;
