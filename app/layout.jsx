9e9cb8546549c3ea455e1ed9aa472c4dd777e477
Yuri Souza <yurdesou@gmail.com>
2023-05-16 22:00:27 -0300

    refactor(RootLayout): Remove redux store provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,14 @@
 import { Header } from '@/components/layout/header/Header';
 
-import { StoreProvider } from '@/components/providers/StoreProvider';
-
 import '../styles/globals.css';
-
-/*
-  Idea for persistence: 
-
-  1. Call cookies here;
-  2. Pass them to a ContextProvider;
-  3. Use the cookies we aquired as their initial state;
-  4. GG.
-
-*/ 
 
 const RootLayout = ({ children }) => {
   return (
     <html>
       <head />
       <body className='pt-20'>
-        <StoreProvider>
-          <Header />
-          {children}
-        </StoreProvider>
+        <Header />
+        {children}
       </body>
     </html>
   );
