ffa0aeffaa380aa2a8b7ee9fc30de4ae35563f3d
Yuri Souza <yurdesou@gmail.com>
2023-05-29 22:15:03 -0300

    refac(cookies.js): Remove unecessary comment

--- app/actions/cookies/cookies.js
+++ app/actions/cookies/cookies.js
@@ -1,12 +1,11 @@
 'use server';
 
 import { cookies } from 'next/headers';
-// import { revalidatePath } from 'next/cache';
 
 export async function storeCookie(name, value) {
-  await cookies().set(name, value);
+  cookies().set(name, value);
 }
 
 export async function retrieveCookie(name) {
-  return cookies().get(name);
+  cookies().get(name);
 }
