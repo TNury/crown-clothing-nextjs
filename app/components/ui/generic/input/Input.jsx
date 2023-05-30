ec74316696f38f1aeba9eb7edc08c642ac850cf6
Yuri Souza <yurdesou@gmail.com>
2023-05-29 22:15:49 -0300

    refac(Input.jsx): Reintroduce client side functionality

--- app/components/ui/generic/input/Input.jsx
+++ app/components/ui/generic/input/Input.jsx
@@ -1,8 +1,21 @@
 export const Input = (props) => {
+  const { id, type, value, onChange, error, placeholder, helperText } = props;
+
   return (
-    <input
-      {...props}
-      className='border-b border-b-black py-2 text-lg outline-none invalid:border-red-500 invalid:bg-red-500 invalid:bg-opacity-20'
-    />
+    <div className='flex flex-col gap-2'>
+      <div className='relative flex flex-col justify-center'>
+        <input
+          id={id}
+          type={type}
+          className='peer/input border-b border-b-black p-2 text-xl outline-none'
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+        />
+      </div>
+      {error && (
+        <span className='block text-base text-red-500'>{helperText}</span>
+      )}
+    </div>
   );
 };
