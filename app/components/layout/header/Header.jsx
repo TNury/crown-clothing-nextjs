833554d8c540896d1076145529184376a78319cb
Yuri Souza <yurdesou@gmail.com>
2023-05-30 20:58:47 -0300

    feat(Header.jsx): Render sign-out or sign-in button based on userSession

--- app/components/layout/header/Header.jsx
+++ app/components/layout/header/Header.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 import CartPreview from '@/components/CartPreview';
 
-export const Header = () => {
+export const Header = ({ userSession }) => {
   const [openCartPreview, setOpenCartPreview] = useState(false);
 
   const toggleCartPreview = () => {
@@ -18,36 +18,36 @@ export const Header = () => {
   };
 
   return (
-    <header className='pl-4 w-full h-20 flex items-center justify-between fixed top-0 z-20 bg-white md:pl-16 md:pr-14'>
+    <header className='fixed top-0 z-20 flex h-20 w-full items-center justify-between bg-white pl-4 md:pl-16 md:pr-14'>
       <Link href='/'>
-        <img src='/assets/logo.svg' alt='brand_logo' className='w-14 h-14' />
+        <img src='/assets/logo.svg' alt='brand_logo' className='h-14 w-14' />
       </Link>
-      <div className='flex gap-2 items-center'>
-        <Link href='/shop' className='p-2 text-base cursor-pointer'>
+      <div className='flex items-center gap-2'>
+        <Link href='/shop' className='cursor-pointer p-2 text-base'>
           SHOP
         </Link>
-        {false ? (
+        {userSession.accessToken ? (
           <button
             onClick={handleSignOutClick}
-            className='p-2 text-base cursor-pointer'
+            className='cursor-pointer p-2 text-base'
           >
             SIGN OUT
           </button>
         ) : (
-          <Link href='/auth' className='p-2 text-base cursor-pointer'>
+          <Link href='/auth' className='cursor-pointer p-2 text-base'>
             SIGN IN
           </Link>
         )}
         <button
           onClick={toggleCartPreview}
-          className='p-2 cursor-pointer flex items-center justify-center relative'
+          className='relative flex cursor-pointer items-center justify-center p-2'
         >
           <img
             src='/assets/shopping-bag.svg'
             alt='bag_icon'
-            className='w-6 h-6'
+            className='h-6 w-6'
           />
-          <span className='text-[10px] font-bold absolute top-4'>
+          <span className='absolute top-4 text-[10px] font-bold'>
             {/* {cartProps.itemsQuantity} */}0
           </span>
         </button>
