ffeb83b97f72e7841e4b56bdf4498b556948c922
Yuri Souza <yurdesou@gmail.com>
2023-05-30 20:55:23 -0300

    feat(layout.jsx): Implement retrieve user session functionality on RootLayout, pass its props to Header

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,17 @@
 import { Header } from '@/components/layout/header/Header';
 
+import { retrieveCookie } from './actions/cookies/cookies';
+
 import '../styles/globals.css';
 
-const RootLayout = ({ children }) => {
+const RootLayout = async ({ children }) => {
+  const userSession = await retrieveCookie('userSession') || {};
+
   return (
     <html>
       <head />
       <body className='pt-20'>
-        <Header />
+        <Header userSession={userSession} />
         {children}
       </body>
     </html>
