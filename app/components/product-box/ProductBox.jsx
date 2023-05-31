1eb975081705a47179417bb3edbba65024415259
Yuri Souza <yurdesou@gmail.com>
2023-05-30 21:25:19 -0300

    chore(ProductBox): Tidy up classNames

--- app/components/product-box/ProductBox.jsx
+++ app/components/product-box/ProductBox.jsx
@@ -4,13 +4,13 @@ import AddToCart from '../ui/specialized/add-to-cart/AddToCart';
 
 export const ProductBox = ({ productData }) => {
   return (
-    <div className='w-full h-full flex flex-col gap-4'>
-      <div className='group w-full h-5/6 border border-black relative cursor-pointer'>
+    <div className='flex h-full w-full flex-col gap-4'>
+      <div className='group relative h-5/6 w-full cursor-pointer border border-black'>
         <img
           src={productData.featuredImage.src}
-          className='w-full h-full object-cover'
+          className='h-full w-full object-cover'
         />
-        <div className='py-4 px-8 flex items-end absolute inset-0 bg-opaque-white-30 opacity-0 group-hover:opacity-100 transition-all duration-200'>
+        <div className='absolute inset-0 flex items-end bg-opaque-white-30 py-4 px-8 opacity-0 transition-all duration-200 group-hover:opacity-100'>
           <AddToCart productData={productData} />
         </div>
       </div>
