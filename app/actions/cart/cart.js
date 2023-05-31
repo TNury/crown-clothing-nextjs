2fa28abe6506df4d41b05b3c38857ab54170322a
Yuri Souza <yurdesou@gmail.com>
2023-05-30 21:23:49 -0300

    feat: Create cart.js actions file, add triggerItemAddition function

---
+++ app/actions/cart/cart.js
@@ -0,0 +1,36 @@
+'use server';
+
+import callAPI from '@services/api';
+
+import { retrieveCookie, storeCookie } from '@/actions/cookies/cookies';
+
+export async function triggerItemAddition(productData) {
+  const merchandiseId = productData.variants.nodes[0].id;
+
+  const existingCartSession = await retrieveCookie('cartSession');
+
+  if (!existingCartSession) {
+    const response = await callAPI(
+      'services/queries/cart.graphql',
+      'createCart',
+      {
+        quantity: 1,
+        merchandiseId,
+      }
+    );
+
+    storeCookie('cartSession', response.cartCreate.cart);
+  } else {
+    const response = await callAPI(
+      'services/queries/cart.graphql',
+      'addItemToCart',
+      {
+        cartId: existingCartSession.id,
+        quantity: 1,
+        merchandiseId,
+      }
+    );
+
+    storeCookie('cartSession', response.cartLinesAdd.cart);
+  }
+}
