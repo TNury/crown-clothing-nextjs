f69ecc8c0f222277f8ab4ed4a240b52d9910d8b0
Yuri Souza <yurdesou@gmail.com>
2023-06-05 20:39:24 -0300

    feat(AuthButton.jsx): Implement logoutUser action

--- app/components/layout/header/auth-button/AuthButton.jsx
+++ app/components/layout/header/auth-button/AuthButton.jsx
@@ -1,15 +1,22 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import Link from 'next/link';
 
 import { logoutUser } from '@/actions/auth/auth';
 
 export const AuthButton = ({ userSession }) => {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await logoutUser(userSession.accessToken);
+
+    router.refresh();
+  };
+
   return userSession.accessToken ? (
-    <button
-      onClick={() => logoutUser(userSession.accessToken)}
-      className='cursor-pointer p-2 text-base'
-    >
+    <button onClick={handleLogout} className='cursor-pointer p-2 text-base'>
       SIGN OUT
     </button>
   ) : (
