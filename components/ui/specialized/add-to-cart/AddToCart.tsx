cc03851f8e23ee516c33d9eb075253d74ba12dd2
Yuri Souza <yurdesou@gmail.com>
2023-06-05 21:29:12 -0300

    chore: Convert AddToCart to typescript

--- components/ui/specialized/add-to-cart/AddToCart.tsx
+++ components/ui/specialized/add-to-cart/AddToCart.tsx
@@ -4,7 +4,7 @@ import { triggerItemAddition } from '@/actions/cart/cart';
 
 import { Button } from '@/components/ui/generic/button/Button';
 
-const AddToCart = ({ productData }) => {
+const AddToCart = ({ productData }: any) => {
   return (
     <Button type='submit' onClick={() => triggerItemAddition(productData)}>
       Add to cart
