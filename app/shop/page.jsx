c73e331025b420d7cc3028a5b7ca3c58fcb6e5e4
Yuri Souza <yurdesou@gmail.com>
2023-06-07 07:51:50 -0300

    chore(Shop): Swap flexbox to grid as layout solution

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -1,8 +1,7 @@
 import Link from 'next/link';
 
-import { ProductBox } from 'components/product-box/ProductBox';
-
 import callAPI from '@services/api';
+import { ProductBox } from 'components/product-box/ProductBox';
 
 const ShopPage = async () => {
   const { collections: shopPageQuery } = await callAPI(
@@ -11,20 +10,17 @@ const ShopPage = async () => {
   );
 
   return (
-    <main id='shop' className='px-4 py-16 flex flex-col gap-16 md:px-16'>
+    <main id='shop' className='flex flex-col gap-16 px-4 py-16 md:px-16'>
       {shopPageQuery.nodes.map((entry, index) => (
         <section key={index} className='flex flex-col gap-4'>
           <Link
             href={`shop/${entry.handle}`}
-            className='text-3xl font-bold uppercase cursor-pointer'
-          >
+            className='cursor-pointer text-3xl font-bold uppercase'>
             {entry.title}
           </Link>
-          <div className='flex gap-4 flex-wrap'>
-            {entry.products.nodes.map((productData) => (
-              <div key={productData.id} className='h-[25vw] flex-auto'>
-                <ProductBox productData={productData} />
-              </div>
+          <div className='grid w-full grid-cols-4 gap-4'>
+            {entry.products.nodes.map((productData, index) => (
+              <ProductBox key={index} productData={productData} />
             ))}
           </div>
         </section>
