d1f16282a07a4ac6720a06d7bfe1c66f693bdaf0
Yuri Souza <yurdesou@gmail.com>
2023-06-08 09:49:50 -0300

    chore(codegen.ts): Add correct plugins

--- codegen.ts
+++ codegen.ts
@@ -5,22 +5,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config: CodegenConfig = {
-  overwrite: true,
-  schema: {
-    'https://nextjs13-crown-clothing.myshopify.com/api/2023-04/graphql.json': {
-      headers: {
-        'X-Shopify-Storefront-Access-Token': process.env.NEXT_PUBLIC_API_KEY,
-      },
+  schema: [
+    {
+      'https://nextjs13-crown-clothing.myshopify.com/api/2023-04/graphql.json':
+        {
+          headers: {
+            'X-Shopify-Storefront-Access-Token':
+              process.env.NEXT_PUBLIC_API_KEY,
+          },
+        },
     },
-  },
+  ],
   documents: 'services/queries/*.graphql',
   generates: {
-    'services/graphql-types/': {
-      preset: 'client',
-      plugins: [],
-    },
-    './graphql.schema.json': {
-      plugins: ['introspection'],
+    './types/queries/queries.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-document-nodes',
+      ],
     },
   },
 };
