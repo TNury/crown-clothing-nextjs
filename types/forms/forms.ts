8e719e254895e84f0b08363364fc9d920c5e0532
Yuri Souza <yurdesou@gmail.com>
2023-06-13 20:22:14 -0300

    feat(forms): Add SignUpFormProps type

--- types/forms/forms.ts
+++ types/forms/forms.ts
@@ -1,4 +1,12 @@
 export type SignInFormProps = {
   email: string;
   password: string;
-}
+};
+
+export type SignUpFormProps = {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  confirmPassword: string;
+};
