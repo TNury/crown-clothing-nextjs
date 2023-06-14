6b9c66fbd62e9d049ae13b66c759b4f7c18f0530
Yuri Souza <yurdesou@gmail.com>
2023-06-13 21:53:14 -0300

    chore(ProductBox): Integrate Image component

--- components/ui/specialized/product-box/ProductBox.tsx
+++ components/ui/specialized/product-box/ProductBox.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import AddToCart from '@/components/ui/specialized/add-to-cart/AddToCart';
 
 import { formatPrice } from '@/utils/misc/formatPrice';
@@ -10,12 +12,16 @@ type ProductBoxProps = {
 
 export const ProductBox: React.FC<ProductBoxProps> = ({ productData }) => {
   return (
-    <div className='flex h-full w-full flex-col gap-4'>
-      <div className='group relative h-5/6 w-full cursor-pointer border border-black'>
-        <img
-          src={productData.featuredImage.url}
-          className='h-full w-full object-cover'
-        />
+    <div className='flex w-full flex-col gap-4'>
+      <div className='group relative w-full cursor-pointer border border-black'>
+        <div className='h-[25vw] w-full relative'>
+          <Image
+            src={productData.featuredImage.url}
+            alt={productData.featuredImage.altText}
+            className='object-cover'
+            fill
+          />
+        </div>
         <div className='absolute inset-0 flex items-end bg-opaque-white-30 px-8 py-4 opacity-0 transition-all duration-200 group-hover:opacity-100'>
           <AddToCart productData={productData} />
         </div>
