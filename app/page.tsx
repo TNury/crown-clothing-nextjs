02e02dc8127191b33b828e647f7f3dbeb72c32a8
Yuri Souza <yurdesou@gmail.com>
2023-06-13 21:46:20 -0300

    chore(Home): Implement Image component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import callAPI from '@/services/api';
@@ -23,10 +24,11 @@ const Home = async () => {
             href={`shop/${entry.handle}`}
             key={entry.id}
             className='group relative flex h-60 min-w-[30%] flex-auto cursor-pointer items-center justify-center overflow-hidden border border-black xl:h-80 [&:nth-child(4)]:flex-1 [&:nth-child(5)]:flex-1'>
-            <img
+            <Image
               src={entry.image.url}
               alt={entry.image.altText}
-              className='absolute -z-10 h-full w-full object-cover transition-all duration-1000 group-hover:scale-110'
+              className='-z-10 object-cover transition-all duration-1000 group-hover:scale-110'
+              fill
             />
             <div className='flex h-24 flex-col items-center gap-4 border border-black bg-white px-6 opacity-70 transition-all duration-500 group-hover:opacity-90'>
               <h2 className='text-2xl font-bold uppercase'>{entry.title}</h2>
