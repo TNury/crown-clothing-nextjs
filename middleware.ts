9667b758b2741c06e3cbf670c597e0456fd13eb0
Yuri Souza <yurdesou@gmail.com>
2023-06-13 21:24:28 -0300

    feat: Create middleware.ts, add conditions for redirecting to homepage

---
+++ middleware.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+/*
+  The middleware function is called for every request
+  that matches the config matcher (see below). It runs
+  before the request handler for the page or API route.
+*/
+export async function middleware(request: NextRequest) {
+  const userSession = request.cookies.get('userSession');
+
+  const nextURL = request.nextUrl.pathname;
+
+  // If the user tries to access auth page while logged in
+  // redirect him to homepage
+  if (nextURL === '/auth' && userSession?.value) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  // If the user tries to access checkout page while not logged in
+  // redirect him to homepage
+  if (nextURL === '/checkout' && !userSession?.value) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - assets/images
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|assets/images).*)',
+  ],
+};
