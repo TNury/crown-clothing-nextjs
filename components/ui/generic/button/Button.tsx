fad00d7d78593a6d18d7c198cdb442ec9faf2f2f
Yuri Souza <yurdesou@gmail.com>
2023-06-14 22:13:04 -0300

    chore(Button): Refactor way we set and handle button variants

--- components/ui/generic/button/Button.tsx
+++ components/ui/generic/button/Button.tsx
@@ -1,30 +1,37 @@
 'use client';
 
-interface ButtonProps {
-  variant?: string;
-  children: React.ReactNode;
-  type: 'button' | 'submit';
-  onClick?: () => void;
-}
+import { type VariantProps, cva } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils/utils';
+
+const buttonVariants = cva(
+  'w-full p-4 flex items-center justify-center transition-all duration-200',
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-black border border-black text-base font-bold uppercase text-white hover:bg-white hover:text-black',
+        secondary:
+          'bg-opaque-white-90 border border-black text-base font-bold uppercase text-black hover:bg-black hover:text-white',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+);
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  const { variant, children, type, onClick } = props;
-
-  const handleClassNames = () => {
-    const standardClasses =
-      'w-full p-4 flex items-center justify-center transition-all duration-200';
-
-    switch (variant) {
-      case 'secondary':
-        return `${standardClasses} bg-opaque-white-90 border border-black text-base font-bold uppercase text-black hover:bg-black hover:text-white`;
-      case 'primary':
-      default:
-        return `${standardClasses} bg-black border border-black text-base font-bold uppercase text-white hover:bg-white hover:text-black`;
-    }
-  };
+  const { children, className, variant, ...restOfProps } = props;
 
   return (
-    <button type={type} className={handleClassNames()} onClick={onClick}>
+    <button
+      className={cn(buttonVariants({ variant, className }))}
+      {...restOfProps}>
       {children}
     </button>
   );
