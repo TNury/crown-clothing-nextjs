6b55f389bb7f03fe696ba8adffbb3c70b26844c6
Yuri Souza <yurdesou@gmail.com>
2023-06-19 21:22:59 -0300

    feat(Input): Extend to enable variant functionality

--- components/ui/generic/input/Input.tsx
+++ components/ui/generic/input/Input.tsx
@@ -1,15 +1,47 @@
-interface InputProps {
-  id: string;
-  type: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  error: boolean;
-  placeholder: string;
-  helperText: string;
+'use client';
+
+import { type VariantProps, cva } from 'class-variance-authority';
+import { FormikHandlers } from 'formik';
+
+import { cn } from '@/lib/utils/utils';
+
+const inputVariants = cva(
+  'border-b bg-gray-1 outline-none p-4 text-base transition-all duration-200 disabled:opacity-70 disabled:pointer-events-none',
+  {
+    variants: {
+      variant: {
+        default:
+          'border-b-black bg-gray-1 outline-none placeholder:text-gray-3',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+);
+
+interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {
+  inputClassName?: string;
+  error: boolean | undefined;
+  onChange: FormikHandlers['handleChange'];
+  helperText: string | false | undefined;
 }
 
 export const Input: React.FC<InputProps> = (props) => {
-  const { id, type, value, onChange, error, placeholder, helperText } = props;
+  const {
+    id,
+    type,
+    value,
+    onChange,
+    error,
+    placeholder,
+    helperText,
+    variant,
+    inputClassName,
+    disabled,
+  } = props;
 
   return (
     <div className='flex flex-col gap-2'>
@@ -17,7 +49,8 @@ export const Input: React.FC<InputProps> = (props) => {
         <input
           id={id}
           type={type}
-          className='border-b border-b-black py-2 text-base outline-none'
+          disabled={disabled}
+          className={cn(inputVariants({ variant, className: inputClassName }))}
           value={value}
           onChange={onChange}
           placeholder={placeholder}
