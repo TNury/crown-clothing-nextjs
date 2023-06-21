6afd30364cf86771c1c6e5d746733ed66bcf65c9
Yuri Souza <yurdesou@gmail.com>
2023-06-21 20:54:48 -0300

    feat(components): Create CartPreviewItem

---
+++ components/layout/header/cart-button/cart-preview/cart-preview-item/CartPreviewItem.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+import { CartItemProps } from '@/types/cart/cart';
+
+export const CartPreviewItem: React.FC = (props: CartItemProps) => {
+  return (
+    <div className='flex w-full'>
+      <Image
+        src={props.merchandise.image.url}
+        width={60}
+        height={80}
+        alt={props.merchandise.image.altText}
+      />
+
+      <div className='p-4 text-left text-base'>
+        <p className='line-clamp-1'>{props.merchandise.product.title}</p>
+        <p>{props.quantity} x €11</p>
+      </div>
+    </div>
+  );
+};
