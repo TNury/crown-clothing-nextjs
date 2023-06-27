5ef09581f96673e4334660b547be3072a7792285
Yuri Souza <yurdesou@gmail.com>
2023-06-26 21:11:15 -0300

    feat(Header): Added new navigation links

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -16,16 +16,35 @@ export const Header = async () => {
     (await retrieveCookie('cartSession')) || {};
 
   return (
-    <header className='fixed top-0 z-20 flex h-20 w-full items-center justify-between bg-white pl-4 md:pl-16 md:pr-14'>
-      <Link href='/'>
-        <div className='relative h-14 w-14'>
-          <Image src='/assets/logo.svg' alt='brand_logo' fill />
-        </div>
+    <header className='fixed top-0 z-20 flex h-20 w-full items-center justify-center bg-white pl-4'>
+      <Link href='/' className='absolute left-8 h-14 w-14'>
+        <Image src='/assets/logo.svg' alt='brand_logo' fill />
       </Link>
-      <div className='flex items-center gap-2'>
-        <Link href='/shop' className='cursor-pointer p-2 text-base'>
-          SHOP
+      <div className='flex gap-2'>
+        <Link
+          href='/shop/womens'
+          className='cursor-pointer p-2 text-base font-bold'>
+          WOMENS
         </Link>
+        <Link
+          href='/shop/mens'
+          className='cursor-pointer p-2 text-base font-bold'>
+          MENS
+        </Link>
+        <Link href='/shop/hats' className='cursor-pointer p-2 text-base'>
+          HATS
+        </Link>
+        <Link href='/shop/jackets' className='cursor-pointer p-2 text-base'>
+          JACKETS
+        </Link>
+        <Link href='/shop/sneakers' className='cursor-pointer p-2 text-base'>
+          SNEAKERS
+        </Link>
+        <Link href='/shop' className='cursor-pointer p-2 text-base'>
+          ALL
+        </Link>
+      </div>
+      <div className='absolute right-8 flex items-center gap-2'>
         <AuthButton userSession={userSession} />
         <CartButton cartSession={cartSession} />
       </div>
