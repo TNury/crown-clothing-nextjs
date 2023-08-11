c87af047144bdc322b987c4e3047f32b8b12ed0a
Yuri Souza <yurdesou@gmail.com>
2023-08-10 21:49:56 -0300

    feat: Update types and styles for OptionsSelector

--- components/ui/generic/options-selector/OptionsSelector.tsx
+++ components/ui/generic/options-selector/OptionsSelector.tsx
@@ -9,7 +9,7 @@ import { Chevron } from '../icons/Chevron';
 export type Option = Record<string, any>;
 
 export interface OptionsSelectorProps {
-  value: string;
+  value: string | number;
   placeholder: string;
   options: Option[];
   optionLabelPath: string;
@@ -46,24 +46,23 @@ export const OptionsSelector: React.FC<OptionsSelectorProps> = (props) => {
   return (
     <div
       id={`option_selector_${placeholder}`}
-      className='relative flex flex-col gap-2 text-left text-base text-black'>
+      className='relative flex w-full flex-col gap-2 text-left text-base text-black'>
       <div
         aria-disabled={disabled}
         className='relative cursor-pointer transition-all duration-200 aria-disabled:pointer-events-none aria-disabled:opacity-50'>
-        <div className='group relative flex h-full w-full items-center justify-between hover:text-white'>
-          <div
-            className='flex w-full items-center gap-4 border border-black bg-white p-4 transition-all duration-200 ease-in-out group-hover:bg-black'
-            onClick={toggleOptions}>
-            <p>{value ? value : placeholder}</p>
-          </div>
+        <div
+          className='group relative flex h-full w-full items-center justify-between gap-4 border border-black bg-white p-4 transition-all duration-200 ease-in-out hover:bg-black hover:text-white'
+          onClick={toggleOptions}>
+          <p className='text-base'>{value ? value : placeholder}</p>
           <Chevron
             data-rotate={showOptions}
-            className='absolute right-4 h-6 w-6 text-black transition-all duration-200 ease-in-out group-hover:text-white data-[rotate=true]:rotate-180'
+            className='h-6 w-6 text-black transition-all duration-200 ease-in-out group-hover:text-white data-[rotate=true]:rotate-180'
           />
         </div>
         {showOptions && (
           <Options
             parentId={`option_selector_${placeholder}`}
+            selectedOption={value}
             options={options}
             optionLabelPath={optionLabelPath}
             handleOptionClick={handleOptionClick}
