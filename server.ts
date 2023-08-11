0a06ca7e725f2b6f42ccbd11cdacd363be981a94
Yuri Souza <yurdesou@gmail.com>
2023-08-10 22:01:42 -0300

    feat: Make production run on port from .env

---
+++ server.ts
@@ -0,0 +1,7 @@
+const cli = require('next/dist/cli/next-start');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+cli.nextStart(['-p', process.env.PORT]);
