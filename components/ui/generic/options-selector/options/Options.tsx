3d3811194b6df31e87898bb7b29da856b63c39d9
Yuri Souza <yurdesou@gmail.com>
2023-08-10 21:50:16 -0300

    feat: On Options component, prevent selected option from showing

--- components/ui/generic/options-selector/options/Options.tsx
+++ components/ui/generic/options-selector/options/Options.tsx
@@ -6,6 +6,7 @@ import type { Option, OptionsSelectorProps } from '../OptionsSelector';
 
 interface OptionsProps {
   parentId: string;
+  selectedOption: string | number;
   options: OptionsSelectorProps['options'];
   optionLabelPath: OptionsSelectorProps['optionLabelPath'];
   handleOptionClick: (optionProps: Record<string, any>) => void;
@@ -13,26 +14,39 @@ interface OptionsProps {
 }
 
 export const Options = (props: OptionsProps) => {
-  const { parentId, optionLabelPath, handleOptionClick, setShowOptions } =
-    props;
+  const {
+    parentId,
+    selectedOption,
+    options,
+    optionLabelPath,
+    handleOptionClick,
+    setShowOptions,
+  } = props;
 
   useClickOutside({
     elementId: parentId,
     onOutsideClick: () => setShowOptions(false),
   });
 
+  const returnOptionLabelPath = (option: Option): string => {
+    return optionLabelPath.split('.').reduce((obj, key) => obj[key], option);
+  };
+
   return (
     <div className='absolute z-10 max-h-56 w-full overflow-auto border border-t-0 border-black bg-white'>
-      {props.options.map((entry: Option, index: number) => (
-        <div
-          key={index}
-          onClick={() => handleOptionClick(entry)}
-          className='border-b border-black p-4 transition-all duration-200 ease-in-out last:border-b-0 hover:bg-black hover:text-white'>
-          <p className='transition-all duration-200 ease-in-out'>
-            {optionLabelPath.split('.').reduce((obj, key) => obj[key], entry)}
-          </p>
-        </div>
-      ))}
+      {options.map(
+        (entry: Option, index: number) =>
+          returnOptionLabelPath(entry) !== selectedOption && (
+            <div
+              key={index}
+              onClick={() => handleOptionClick(entry)}
+              className='border-b border-black p-4 transition-all duration-200 ease-in-out last:border-b-0 hover:bg-black hover:text-white'>
+              <p className='transition-all duration-200 ease-in-out'>
+                {returnOptionLabelPath(entry)}
+              </p>
+            </div>
+          )
+      )}
     </div>
   );
 };
