62a09a9029c074ce6d5be7753187709d619416b0
Yuri Souza <yurdesou@gmail.com>
2023-09-21 00:24:00 -0300

    feat: Implement loading and error handling on PaymentFormGroup

--- components/ui/specialized/payment-form-group/PaymentFormGroup.tsx
+++ components/ui/specialized/payment-form-group/PaymentFormGroup.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+
 import {
   CardCvcElement,
   CardExpiryElement,
@@ -9,6 +11,7 @@ import {
 } from '@stripe/react-stripe-js';
 import { v4 as uuidv4 } from 'uuid';
 
+import { Alert } from '@/components/ui/generic/alert/Alert';
 import { Button } from '@/components/ui/generic/button/Button';
 
 import { completeCheckout } from '@/actions/checkout/checkout.actions';
@@ -29,10 +32,15 @@ type PaymentFormGroupProps = {
 export const PaymentFormGroup: React.FC<PaymentFormGroupProps> = ({
   checkoutSession,
 }) => {
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
   const stripe = useStripe();
   const elements = useElements();
 
   const triggerPayment = async () => {
+    setLoading(true);
+
     if (!stripe || !elements) {
       // In case Stripe.js has not yet loaded.
       return;
@@ -46,6 +54,7 @@ export const PaymentFormGroup: React.FC<PaymentFormGroupProps> = ({
 
     if (tokenResult.error) {
       console.log(tokenResult.error.message);
+      setErrorMessage(tokenResult.error.message);
     } else {
       await completeCheckout(checkoutSession.id, {
         type: PaymentTokenType.StripeVaultToken,
@@ -61,27 +70,39 @@ export const PaymentFormGroup: React.FC<PaymentFormGroupProps> = ({
         paymentData: tokenResult.token.id,
       });
     }
+
+    setLoading(false);
   };
 
   return (
-    <div className='flex w-1/2 flex-col gap-10'>
-      <div className='flex flex-col gap-4'>
-        <div className='flex flex-col gap-2'>
-          <p className='text-base'>Card number</p>
-          <CardNumberElement options={cardNumberElementOptions} />
-        </div>
-        <div className='flex gap-4'>
-          <div className='flex w-full flex-col gap-2'>
-            <p className='text-base'>Expiration</p>
-            <CardExpiryElement options={cardExpiryElementOptions} />
+    <>
+      <div
+        aria-disabled={loading}
+        className='flex w-1/2 flex-col gap-10 transition-all duration-200 aria-disabled:pointer-events-none aria-disabled:opacity-50'>
+        <div className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-2'>
+            <p className='text-base'>Card number</p>
+            <CardNumberElement options={cardNumberElementOptions} />
           </div>
-          <div className='flex w-full flex-col gap-2'>
-            <p className='text-base'>CVC</p>
-            <CardCvcElement options={cardCvcElementOptions} />
+          <div className='flex gap-4'>
+            <div className='flex w-full flex-col gap-2'>
+              <p className='text-base'>Expiration</p>
+              <CardExpiryElement options={cardExpiryElementOptions} />
+            </div>
+            <div className='flex w-full flex-col gap-2'>
+              <p className='text-base'>CVC</p>
+              <CardCvcElement options={cardCvcElementOptions} />
+            </div>
           </div>
         </div>
+        <Button onClick={triggerPayment}>Pay</Button>
       </div>
-      <Button onClick={triggerPayment}>Pay</Button>
-    </div>
+      <Alert
+        open={Boolean(errorMessage)}
+        message={errorMessage}
+        variant='error'
+        onClose={() => setErrorMessage('')}
+      />
+    </>
   );
 };
